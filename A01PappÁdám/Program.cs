7539114fb14102e32d77ac600e7134f87be8b165
PappAdam0 <papp.adaam@gmail.com>
2020-09-14 10:49:52 +0200

    Add project files.

---
+++ A01PappÁdám/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace A01PappÁdám
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
